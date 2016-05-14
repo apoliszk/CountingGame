a61053a84370690da41563a87e20e82fe73efc39
apoliszk <apolis@qq.com>
2016-05-14 21:58:59 +0800

    add game logic

--- app/game.component.js
+++ app/game.component.js
@@ -5,9 +5,28 @@
             templateUrl: 'app/game.component.html'
         })
         .Class({
-            constructor: function() {},
-            onInputNumberChange: function(event) {
-                debugger;
+            constructor: function() {
+                this.number = 7;
+                this.tableElementsCount = 200;
+                this.onNumberChange(this.number);
+            },
+            onNumberChange: function(value) {
+                if (/^\d+$/.test(value)) {
+                    this.inputError = false;
+                    this.generateTable();
+                } else {
+                    this.inputError = true;
+                }
+            },
+            generateTable: function() {
+                var arr = [];
+                for (var i = 1; i <= this.tableElementsCount; i++) {
+                    arr.push({
+                        number: i,
+                        skip: i % this.number == 0 || (i + '').indexOf(this.number + '') >= 0
+                    });
+                }
+                this.tableElements = arr;
             }
         });
 })(window.app || (window.app = {}));
